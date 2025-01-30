9e0313a08bf663368bd5a5bc9c49241e7bef7464
felix diaz <felixmdm321@gmail.com>
2025-01-29 17:17:41 -0800

    i think gitignore was messing me up here

---
+++ finwelldiagnosistool/src/data/FooterData.tsx
@@ -0,0 +1,16 @@
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+
+const links = [
+    {
+        icon: <FaLinkedinIn />,
+        link: "https://www.linkedin.com/in/felix-m-diaz/",
+        text: "Linkedin",
+    },
+    {
+        icon: <FaGithub />,
+        link: "https://github.com/FelixMDM/finwell",
+        text: "GitHub",
+    },
+];
+
+export default links;
