af5f92dd6a34f854371f5329f1a60daa8a66ea51
felix diaz <felixmdm321@gmail.com>
2025-01-29 17:04:25 -0800

    redefined imports and renamed module

--- finwelldiagnosistool/src/components/Footer.tsx
+++ finwelldiagnosistool/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import { links } from "@/data/footerdata";
+import { links } from "../data/FooterData";
 
 const Footer = () => {
     return (
